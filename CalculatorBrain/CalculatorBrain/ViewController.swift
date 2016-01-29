7ad3d4767899a11ca29bf4cfbd4ce6d566e1e018
Brian Glowe <brianglowe@gmail.com>
2016-01-28 21:18:06 -0500

    Part 2: Next, we remove the repeated code from the performOperation() in attempt to share some of the code between operations. **EDIT: performOperation() SHOULD BE INSTEAD operation() So, we create a new function called performOperation() give it a type. Swift allows for functions to be declared as a type, in which case we will use the power of Swift to attempt to eliminate some repeated code. We will declare the performOperation() as a type which takes two Doubles as its arguments and returns a Double.  This will allow us to create new functions which will perform each individual operation. After declaring performOperation() and assigning its type, we declare 4 new functions for each mathematical operation (declared as multiply(), divide(), et cetra) While this solution again runs successfully, you are still repeating alot of code and not adhering to the DRY principle.

--- CalculatorBrain/CalculatorBrain/ViewController.swift
+++ CalculatorBrain/CalculatorBrain/ViewController.swift
@@ -30,8 +30,10 @@ class ViewController: UIViewController {
     }
 
 
-// @30:45 - Here the operate() is providing the operations we need, but this is very sloppy code and NOT 
-    // DRY. If someone was to read this code, they would laugh at how poor of programmers I am
+// beginning at @31' - 34:30'
+// We are going to take the operations out of the operate() and create new funcitons to share the code between each
+    // differnt operation. We will declare the performOperation() as a type which take a function whose argumetns are 
+    //two doubles and returns a double
     
     @IBAction func operate(sender: UIButton) {
         let operation = sender.currentTitle!
@@ -39,29 +41,41 @@ class ViewController: UIViewController {
             enter()
         }
         switch operation {
-            case "×":
-                if operandStack.count >= 2 {
-                    displayValue = operandStack.removeLast() * operandStack.removeLast()
-                    enter()
-            }
-            case "÷":
-                if operandStack.count >= 2 {
-                    displayValue = operandStack.removeLast() / operandStack.removeLast()
-                    enter()
-            }
-            case "+":
-                if operandStack.count >= 2 {
-                    displayValue = operandStack.removeLast() + operandStack.removeLast()
-                    enter()
-            }
-            case "−":
-                if operandStack.count >= 2 {
-                    displayValue = operandStack.removeLast() - operandStack.removeLast()
-                    enter()
-            }
+            case "×": performOperation(multiply)
+            case "÷": performOperation(divide)
+            case "+": performOperation(addition)
+//            case "−":
         default: break
         }
     }
+    
+//Let's try to go more DRY by sharing a function among the operations in the operate()
+// WE CAN DO THAT BY DECLARING A FUNCTION AS A TYPE WHICH TAKE EACH INDIVIDUAL OPERATION FUNCTION
+    
+    //1. here we begin by declaring the performOperation() as a type: (Double, Double) -> Double
+    // This function takes a function which has two arguments (each doubles) and returns a double
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    //2. Next each case of the operations are entered below, which the performOperation() will take as its arguments
+    func multiply(op1: Double, op2:Double) ->Double {
+        return op1 * op2
+    }
+
+    func divide(op1: Double, op2:Double) ->Double {
+        return op1 / op2
+    }
+
+    func addition(op1: Double, op2:Double) ->Double {
+        return op1 + op2
+    }
+
+//Now, the problem with the above solution, well, it is just a code heavy and again repeating alot of the same code.
+    // while we are sharing some code, we are repeating much of the same again.
     
     var operandStack = [Double]()
     
@@ -81,4 +95,12 @@ class ViewController: UIViewController {
     }
 
 }
+
+
+
+
+
+
+
+
 
