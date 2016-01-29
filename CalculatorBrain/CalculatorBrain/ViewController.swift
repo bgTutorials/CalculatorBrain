681862c00e96ff6fa9cdf921d8a48a3e5e8feb0a
Brian Glowe <brianglowe@gmail.com>
2016-01-28 22:16:57 -0500

    Part 5: Final And the power of closures reduces the entire CalculatorBrain logic to single lined operations and a performOperation()

--- CalculatorBrain/CalculatorBrain/ViewController.swift
+++ CalculatorBrain/CalculatorBrain/ViewController.swift
@@ -30,7 +30,7 @@ class ViewController: UIViewController {
     }
 
 
-// beginning at @36:16' - 37:11'
+// beginning at @37:11' - 40'
 //Swift's powerful Type Inference allows eliminate the need to declare the types of the passed function's arguments (see: Double)
     
     @IBAction func operate(sender: UIButton) {
@@ -39,23 +39,32 @@ class ViewController: UIViewController {
             enter()
         }
         switch operation {
-    //1. Because Swift knows we are calling performOperation(), it knows the arguments - op1 & op2 - are Double.   
-        case "×": performOperation({ (op1, op2) in return op1 * op2 })
-        case "÷": performOperation({ (op1, op2) in return op1 / op2 })
-        case "+": performOperation({ (op1, op2) in return op1 + op2 })
-//            case "−":
+        // 1. we dont need to express "return" because again Swift infers from the called function that it returns the operation
+        case "×": performOperation({ (op1, op2) in op1 * op2 })
+            
+        //2. Swift does not force you to name the arguments (ie. op1 & op2), 
+        //if you dont name them Swift will use the $0,$1,$2,$3... as the parameters ofthe argument
+        // Note - the change in order of operations
+        case "÷": performOperation({ $1 / $0 })
+            
+        //3. If it is the last argument of the function (performOperation() only has one argument,however) you can put the expression outside the parathesis.
+        case "+": performOperation() { $0 + $1 }
+
+        //4. And if it is the only argument passed to the function (as with performOperation()), we can delete the parenthesis entirely.
+        case "−": performOperation { $1 - $0 }
         default: break
         }
     }
     
-    //2. Because we are calling this function, it knows this function takes two Doubles as its arguments and returns a Double
     func performOperation(operation: (Double, Double) -> Double) {
         if operandStack.count >= 2 {
             displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
             enter()
         }
     }
-        
+
+// Wow, that is some nice, concise code.
+    
     var operandStack = [Double]()
     
     @IBAction func enter() {
